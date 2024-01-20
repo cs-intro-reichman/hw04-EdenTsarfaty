d9e99289c7f6f42a0b2ac443b3775d8ed58cb7d0
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-20 19:00:18 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,22 +1,6 @@
 public class ArrayOps {
     public static void main(String[] args) {
-//        System.out.println(findMissingInt(new int[] {3, 0, 1})); // 2
-//        System.out.println(findMissingInt(new int[] {0, 1, 2, 3, 4, 6})); // 5
-//        System.out.println(findMissingInt(new int[] {0})); // 1
-//        System.out.println(secondMaxValue(new int[] {6, 9, 4, 7, 3, 4})); // 7
-//        System.out.println(secondMaxValue(new int[] {1, 2, 3, 4, 5})); // 4
-//        System.out.println(secondMaxValue(new int[] {2, 8, 3, 7, 8})); // 8
-//        System.out.println(secondMaxValue(new int[] {1, -2, 3, -4, 5})); // 3
-//        System.out.println(secondMaxValue(new int[] {-202, 48, 13, 7, 8})); // 13
-//        System.out.println(containsTheSameElements(new int[] {1, 2, 1, 1, 2}, new int[] {2, 1})); // true
-//        System.out.println(containsTheSameElements(new int[] {2, 2, 3, 7, 8, 3, 2}, new int[] {8, 2, 7, 7, 3})); // true
-//        System.out.println(containsTheSameElements(new int[] {1, 2, 3}, new int[] {1, 2, 3})); // true
-//        System.out.println(containsTheSameElements(new int[] {3, -4, 1, 2, 5}, new int[] {1, 3, -4, 5})); // false
-//        System.out.println(isSorted(new int[] {7, 5, 4, 3, -12})); // true
-//        System.out.println(isSorted(new int[] {1, 2, 3})); // true
-//        System.out.println(isSorted(new int[] {1, -2, 3})); // false
-//        System.out.println(isSorted(new int[] {1, 1, 500})); // true
-//        System.out.println(isSorted(new int[] {1, 3, 2})); // false
+        // #feedback: please dont keep historical code within comments
     }
     
     public static int findMissingInt (int [] array) {
@@ -33,6 +17,7 @@ public class ArrayOps {
     public static int secondMaxValue(int [] array) {
         int max = array[0];
         int maxindex = 0;
+        // #feedback: you could use one loop to find the second (when new max found - second max is the old one)
         for (int i = 1; i < array.length; i++)      //Determines the max value and index
         {
             if (array[i] > max) {
